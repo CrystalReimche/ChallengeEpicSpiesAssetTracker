364a175a6e50c97292467998e67caeabde0dc393
Crystal <CwittleZ@gmail.com>
2018-03-10 14:17:28 -0800

    adding Epic Spies Asset Tracker Challenge

---
+++ ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" ImageUrl="~/epic-spies-logo.jpg" />
+        </div>
+        <h1>Asset Performance Tracker</h1>
+        <p>
+            Asset Name:&nbsp;
+            <asp:TextBox ID="assetTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Elections Rigged:&nbsp;
+            <asp:TextBox ID="electionTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Acts of Subterfuge Performed:&nbsp;
+            <asp:TextBox ID="actsTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="Add Asset" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            // Create empty arrays
+            string[] assetName = new string[0];
+            int[] electionsRigged = new int[0];
+            int[] actsPerformed = new int[0];
+
+            ViewState.Add("AssetName", assetName);
+            ViewState.Add("ElectionsRigged", electionsRigged);
+            ViewState.Add("ActsPerformed", actsPerformed);
+        }
+    }
+
+    protected void addButton_Click(object sender, EventArgs e)
+    {
+        // Retrieve values from ViewState and cast it to arrays of appropriate type
+        string[] assetName = (string[])ViewState["AssetName"];
+        int[] electionsRigged = (int[])ViewState["ElectionsRigged"];
+        int[] actsPerformed = (int[])ViewState["ActsPerformed"];
+
+        // Establish variable for reuse
+        int newLength = assetName.Length + 1;
+
+        // Resize arrays
+        Array.Resize(ref assetName, newLength);
+        Array.Resize(ref electionsRigged, newLength);
+        Array.Resize(ref actsPerformed, newLength);
+
+        // Variable to grab newest index
+        int newIndex = assetName.GetUpperBound(0);
+
+        // Grabbing values that were entered into text boxes and storing them into array
+        assetName[newIndex] = assetTextBox.Text;
+        electionsRigged[newIndex] = int.Parse(electionTextBox.Text);
+        actsPerformed[newIndex] = int.Parse(actsTextBox.Text);
+
+        // Updating the ViewState with most recent added value in text boxes
+        ViewState["AssetName"] = assetName;
+        ViewState["ElectionsRigged"] = electionsRigged;
+        ViewState["ActsPerformed"] = actsPerformed;
+        
+
+        resultLabel.Text = String.Format("Total Elections Rigged: {0}<br>Average Acts of Subterfuge per Asset {1:N2}<br>(Last Asset You Added: {2})",
+            electionsRigged.Sum(),
+            actsPerformed.Average(),
+            assetName.Last());
+    }
+}
